c1d12d7a4e65cbed80b943d72f0ecac10e478243
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-19 09:58:06 +0200

    Updated comments

--- docs/step1.js
+++ docs/step1.js
@@ -1,3 +1,6 @@
+// reference na objekt představující kreslicí plátno umístěné na webové stránce
 const canvas = document.getElementById("web_gl_canvas");
 
+// zobrazení základních informací o získaném objektu
 window.alert("Canvas: " + canvas.width + "x" + canvas.height);
+
